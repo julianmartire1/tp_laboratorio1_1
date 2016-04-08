265b0f14d9021e31afa92e5cde200b76347e9215
julianmartire1 <julianmartire.jm@gmail.com>
2016-04-07 21:06:39 -0300

    x=0 y y=0

--- main.c
+++ main.c
@@ -8,12 +8,12 @@ int main()
 {
     char seguir='s';
     int opcion=0,band=0,band1=0;
-    float x,y;
+    float x=0,y=0;
     while(seguir=='s')
     {
 
-        printf("\n 1- Ingresar 1er operando (A=x)\n");
-        printf("\n 2- Ingresar 2do operando (B=y)\n");
+        printf("\n 1- Ingresar 1er operando (A=%.2f)\n",x);
+        printf("\n 2- Ingresar 2do operando (B=%.2f)\n",y);
         printf("\n 3- Calcular la suma (A+B)\n");
         printf("\n 4- Calcular la resta (A-B)\n");
         printf("\n 5- Calcular la division (A/B)\n");
@@ -23,7 +23,7 @@ int main()
         printf("\n 9- Salir\n");
 
         scanf("%d",&opcion);
-        system("cls");
+
 
 
         switch(opcion)
@@ -32,58 +32,71 @@ int main()
                 printf("Ingrese primer operando: ");
                 scanf("%f",&x);
                 band=1;
+                system("cls");
                 break;
             case 2:
                 printf("Ingrese segundo operando: ");
                 scanf("%f",&y);
                 band1=1;
+                system("cls");
                 break;
             case 3:
+                system("cls");
                 if(band==1 && band1==1)
                 {
                     printf("La suma es: %.2f  \n",suma(x,y));
                 }
+
                 break;
 
 
             case 4:
+                system("cls");
                 if(band==1 && band1==1)
                 {
                     printf("La resta es: %.2f  \n",resta(x,y));
                 }
 
+
                 break;
             case 5:
+                system("cls");
                 if(band==1 && band1==1)
                 {
                     printf("La division es: %.2f",division(x,y));
                 }
+
                 break;
             case 6:
+                system("cls");
                 if(band==1 && band1==1)
                 {
                     printf("La multiplicacion es : %.2f",multiplicacion(x,y));
                 }
+
                 break;
             case 7:
-
+                system("cls");
                 if(band==1)
                 {
                     printf("El factorial es: %.2f",factorial(x));
                 }
+
                 break;
             case 8:
+                system("cls");
                 if(band==1 && band1==1)
                 {
-                    printf("La suma es: %.2f  \n",suma(x,y));
-                    printf("La resta es: %.2f  \n",resta(x,y));
-                    printf("La division es: %.2f \n",division(x,y));
-                    printf("La multiplicacion es : %.2f \n",multiplicacion(x,y));
+                    printf("\n La suma es: %.2f  \n",suma(x,y));
+                    printf("\n La resta es: %.2f  \n",resta(x,y));
+                    printf("\n La division es: %.2f \n",division(x,y));
+                    printf("\n La multiplicacion es : %.2f \n",multiplicacion(x,y));
                 }
                 if(band==1)
                 {
-                    printf("El factorial es: %.2f",factorial(x));
+                    printf("\n El factorial es: %.2f \n",factorial(x));
                 }
+
 
                 break;
             case 9:
